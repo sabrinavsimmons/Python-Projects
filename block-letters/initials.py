19cb92f7229f80ed9e1091db03673521c2b4faac
Sabrina V Simmons <sabrinasimmons32509@gmail.com>
2026-04-30 22:11:13 -0500

    Add Block Letters ASCII art project - Codecademy Python 3

---
+++ block-letters/initials.py
@@ -0,0 +1,10 @@
+# Sabrina V Simmons
+# I love to code!
+
+print(" SSS  V   V  SSS ")
+print("S   S V   V S   S")
+print("S     V   V S    ")
+print(" SSS   V V   SSS ")
+print("    S  V V      S")
+print("S   S   V   S   S")
+print(" SSS    V    SSS ")
